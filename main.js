eef68d430f2629f8365b4547d842f3325cb54bec
bratliff <ben@2bitcreative.com>
2019-01-23 13:30:44 -0500

    updated gitignore

--- main.js
+++ main.js
@@ -1,53 +1,58 @@
-// Define a new component called button-counter
-// Define a new component called button-counter
-  Vue.mixin({ delimiters: ['[[',']]'] });
+ // Define a new component called button-counter
+Vue.mixin({ delimiters: ['[[',']]'] });
 
-Vue.component('amplify-dropdown', {
+Vue.component('amplify-multiselect-dropdown', {
   data: function () {
     return {
       isMenuActive: false,
-      ariaChecked: 'false',
-      items: [
-        {label: 'Teacher'},
-        {label: 'Administrator'},
-        {label: 'Student'},
-        {label: 'Executive Assistant to the Administrative Overseer'},
-        {label: 'Other'}
-      ]
+      ariaChecked: 'false'
     }
   },
+  props: {
+    buttonText: String,
+    items: Array
+  },
   methods: {
-      openMenu: function() {
-        this.isMenuActive = !this.isMenuActive;
+    openMenu: function() {
+      this.isMenuActive = !this.isMenuActive;
 
-        event.target.getAttribute("aria-expanded") == "false" ? event.target.setAttribute("aria-expanded", "true") : event.target.setAttribute("aria-expanded", "false");
-      },
-      closeMenu: function(event) {
-        console.log('close menu called');
-        this.isMenuActive = false;
-        this.$refs.menuBtn.focus();
-        this.$refs.menuBtn.setAttribute("aria-expanded", "false");
-      },
-      checkboxClick: function(event) {
-        event.target.parentNode.getAttribute("aria-checked") == "false" ? event.target.parentNode.setAttribute("aria-checked", "true") : event.target.parentNode.setAttribute("aria-checked", "false");
-      },
-      focusFirstItem: function(event) {
-        event.preventDefault();
-        this.$refs.listItem[0].focus();
-      },
-      chooseItem: function(event) {
-        event.preventDefault();
-        event.target.click();
-        event.target.focus();
-      },
-      clickItem: function(event) {
-        event.target.focus();
-      },
-      preventDefault: function(event){
-        event.preventDefault();
-        event.stopPropagation();
-      },
+      event.target.getAttribute("aria-expanded") == "false"
+        ? event.target.setAttribute("aria-expanded", "true")
+        : event.target.setAttribute("aria-expanded", "false");
     },
+    closeMenu: function() {
+      this.isMenuActive = false;
+      this.$refs.menuBtn.focus();
+      this.$refs.menuBtn.setAttribute("aria-expanded", "false");
+    },
+    moveDown: function(e) {
+      e.target.nextSibling.focus();
+    },
+    moveUp: function(e) {
+      e.target.previousSibling.focus();
+    },
+    checkboxClick: function(event) {
+      event.target.parentNode.getAttribute("aria-checked") == "false"
+        ? event.target.parentNode.setAttribute("aria-checked", "true")
+        : event.target.parentNode.setAttribute("aria-checked", "false");
+    },
+    focusFirstItem: function(event) {
+      event.preventDefault();
+      this.$refs.listItem[0].focus();
+    },
+    chooseItem: function(event) {
+      event.preventDefault();
+      event.target.click();
+      event.target.focus();
+    },
+    clickItem: function(event) {
+      event.target.focus();
+    },
+    preventDefault: function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  },
     created: function(){
         window.addEventListener('click', e => {
           let allmenus = document.getElementsByClassName('amplify-multi-select')[0];
@@ -58,14 +63,15 @@ Vue.component('amplify-dropdown', {
             
         })
       },
-    template: `<div class="amplify-dropdown amplify-multi-select" id="multi-select-menu" v-bind:class="{show: isMenuActive}" ref="menu">
-        <button class="btn multiselect-btn" type="button" aria-haspopup="true" aria-expanded="false" id="multi-select-button" @click="openMenu" @keydown.down="focusFirstItem" ref="menuBtn">Select multiple items</button>
+    template: `<div class="amplify-dropdown amplify-multi-select" v-bind:class="{show: isMenuActive}" ref="menu">
+        <button class="btn multiselect-btn" type="button" aria-haspopup="true" aria-expanded="false" id="multi-select-button" @click="openMenu" @keydown.down="focusFirstItem" ref="menuBtn">[[buttonText]]</button>
         <div class="dropdown-menu" aria-labelledby="multi-select-button">
-            <label v-for="label in items" tabindex="0" class="dropdown-item" role="checkbox" aria-checked="false" ref="listItem" @click="clickItem" @keydown.space.enter="chooseItem" @keydown.esc="closeMenu" @keyup.tab="closeMenu">
+            <label v-for="item in items" :key="item.id" tabindex="0" class="dropdown-item" role="checkbox" aria-checked="false" ref="listItem" @click="clickItem" @keydown.space.enter="chooseItem" @keydown.esc="closeMenu" @keyup.tab="closeMenu" @keydown.down="moveDown" @keydown.up="moveUp">
               <input type="checkbox" value="on" @click="checkboxClick">
               <div class="checkbox-div"></div>
-              <span>[[label.label]]</span>
+              <span>[[item]]</span>
             </label>
         </div>
-        </div>`
-})
+    </div>`
+});
+
